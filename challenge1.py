c4ba7bc781b03f618044415182349ac538640870
littlebigendian <cassandrabv98@gmail.com>
2019-07-14 17:06:13 -0500

    Results are printed as output

--- challenge1.py
+++ challenge1.py
@@ -1,32 +1,44 @@
-#Input of number of test cases
+#Input of numberIndex of test cases
 testCases = int(input())
 #Input must be between 1 and 100.
 if testCases >= 1 and testCases <= 100: #If condition is true then
     repeat = 0 #Iterate each testCase without for loop.
     results = []
+    
     while repeat < testCases:
         #Ask for quantity of numbers contained in each testCase.
         testCaseLen = int(input()) #Must be between 1 and 100.
-        if testCaseLen >= 1and testCaseLen <=100:#If condition is true then
+        if testCaseLen >= 1 and testCaseLen <= 100:#If condition is true then
             numberList = list(map(int, input().split())) #Ask for the numbers as a list. Separated by spaces.
              
-            while len(numberList) == testCaseLen: #Quantity must be the same as input.
-                square = 0
-                sum = 0
-                elements = 0
-                number = 0 #Iterate all elements of list
-                while number < len(numberList): #With the purpose of
-                    if numberList[number] < 0:#If number contained in list is negative
-                        numberList.remove(numberList[number])#Then remove it from list. 
+            if len(numberList) == testCaseLen: #Quantity must be the same as input.
+                
+                numberIndex = 0 #Iterate all elementIndex of list
+                
+                while numberIndex < len(numberList): #With the purpose of
+                    if numberList[numberIndex] < 0:#If numberIndex contained in list is negative
+                        numberList.remove(numberList[numberIndex])#Then remove it from list. 
                         continue #Jump loop
-                    number += 1#add number
-                while elements < len(numberList): #Iterate all elements of list
-                    square = pow(numberList[elements],2) #Power each element ^2
-                    sum = sum + square #The sum of squares of all elements.
-                    elements += 1 #add number
-                print(sum)
-    
+                    numberIndex += 1#add numberIndex
+                elementIndex = 0
+                
+                sum = 0
+                while elementIndex < len(numberList): #Iterate all elementIndex of list
+                    square = pow(numberList[elementIndex],2) #Power each element ^2
+                    sum = sum + square #The sum of squares of all elementIndex.
+                    elementIndex += 1 #add numberIndex
+                results.append(sum)
+            
+        repeat += 1
+    i = 0 
+      
+    while i < len(results):
+        print(results[i])
+        i += 1
+               
+               
 
+               
 
         
 
